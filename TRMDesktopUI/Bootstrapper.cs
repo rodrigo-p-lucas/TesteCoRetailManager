9ba5ce19e96765b76c7a4969568e322face0474b
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-08-26 23:03:33 -0300

    Implementação de DI para UI
    Implementa injeção de dependência usando SimpleContainer existente no Carliburn

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,46 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            container.Instance(container);
+
+            container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => container.RegisterPerRequest(viewModelType, viewModelType.Name, viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
         }
     }
 }
