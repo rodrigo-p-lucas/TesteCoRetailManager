8a8bd714152427a739798ee37496bdf0a53d1604
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-08 22:29:53 -0300

    Montar consulta a banco(parte 01)
    Monta a estrutura inicial de consulta a banco, juntamente com modelos e procedure, e implementação de método na API.

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+BEGIN
+	set nocount on;
+	
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[User]
+	WHERE Id = @Id;
+END
---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IList<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUsersById(userId);
+        }
+    }
+}
