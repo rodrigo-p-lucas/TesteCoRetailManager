b313c015840a1f9697e27c1929f7fbdc1067e16d
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-10 22:27:57 -0300

    Adiciona Sales Page
    Adiciona pagina de vendas usando WPF mas sem regra de negócio.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,113 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+		private BindingList<string>	_products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set 
+			{ 
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private BindingList<string> _cart;
+
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set 
+			{ 
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set 
+			{ 
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		public string SubTotal { 
+			get 
+			{
+				//TODO - Replace with calculation
+				return "$0.00";
+			} 
+		}
+
+		public string Tax
+		{
+			get
+			{
+				//TODO - Replace with calculation
+				return "$0.00";
+			}
+		}
+
+		public string Total
+		{
+			get
+			{
+				//TODO - Replace with calculation
+				return "$0.00";
+			}
+		}
+
+		public bool CanAddToChart {
+			get
+			{
+				bool output = false;
+
+				//Make sure something is selected
+
+				return output;
+			}
+		}
+
+		public void AddtoChart()
+		{
+
+		}
+
+		public bool CanRemoveToChart
+		{
+			get
+			{
+				bool output = false;
+
+				//Make sure something is selected
+
+				return output;
+			}
+		}
+
+		public void RemoveToChart()
+		{
+
+		}
+
+		public void CheckOut()
+		{
+
+		}
+	}
+}
