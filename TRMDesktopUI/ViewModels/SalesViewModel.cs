17d6c669cbb0822818902a26056dfd0e2fbb65a7
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-14 17:17:06 -0300

    Implementar botão AddToCart
    Adiciona funcionalidade no botão AddToCart, exibe subtotal calculado e atualiza a quantidade de item repetidos no cart.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -12,122 +12,169 @@ namespace TRMDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
-		private  IProductEndpoint _productEndpoint;
-		public SalesViewModel(IProductEndpoint productEndpoint)
-		{
-			_productEndpoint = productEndpoint;
-		}
+        private IProductEndpoint _productEndpoint;
+        public SalesViewModel(IProductEndpoint productEndpoint)
+        {
+            _productEndpoint = productEndpoint;
+        }
 
-		protected override async void OnViewLoaded(object view)
-		{
-			base.OnViewLoaded(view);
-			await LoadProducts();
-		}
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            await LoadProducts();
+        }
 
-		private async Task LoadProducts()
-		{
-			var productList = await _productEndpoint.GetAll();
-			Products = new BindingList<ProductModel>(productList);
-		}
+        private async Task LoadProducts()
+        {
+            var productList = await _productEndpoint.GetAll();
+            Products = new BindingList<ProductModel>(productList);
+        }
 
-		private BindingList<ProductModel>	_products;
+        private BindingList<ProductModel> _products;
 
-		public BindingList<ProductModel> Products
-		{
-			get { return _products; }
-			set 
-			{ 
-				_products = value;
-				NotifyOfPropertyChange(() => Products);
-			}
-		}
+        public BindingList<ProductModel> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
 
-		private BindingList<ProductModel> _cart;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
 
-		public BindingList<ProductModel> Cart
-		{
-			get { return _cart; }
-			set 
-			{ 
-				_cart = value;
-				NotifyOfPropertyChange(() => Cart);
-			}
-		}
+        public BindingList<CartItemModel> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
 
-		private int _itemQuantity;
+        private int _itemQuantity = 1;
 
-		public int ItemQuantity
-		{
-			get { return _itemQuantity; }
-			set 
-			{ 
-				_itemQuantity = value;
-				NotifyOfPropertyChange(() => ItemQuantity);
-			}
-		}
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
 
-		public string SubTotal { 
-			get 
-			{
-				//TODO - Replace with calculation
-				return "$0.00";
-			} 
-		}
+        private ProductModel _selectedProduct;
 
-		public string Tax
-		{
-			get
-			{
-				//TODO - Replace with calculation
-				return "$0.00";
-			}
-		}
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
 
-		public string Total
-		{
-			get
-			{
-				//TODO - Replace with calculation
-				return "$0.00";
-			}
-		}
 
-		public bool CanAddToChart {
-			get
-			{
-				bool output = false;
+        public string SubTotal
+        {
+            get
+            {
+                decimal subtotal = 0.0m;
 
-				//Make sure something is selected
+                foreach (var item in Cart)
+                {
+                    subtotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
 
-				return output;
-			}
-		}
+                return subtotal.ToString("C");
+            }
+        }
 
-		public void AddtoChart()
-		{
+        public string Tax
+        {
+            get
+            {
+                //TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
 
-		}
+        public string Total
+        {
+            get
+            {
+                //TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
 
-		public bool CanRemoveToChart
-		{
-			get
-			{
-				bool output = false;
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
 
-				//Make sure something is selected
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
-				return output;
-			}
-		}
+                return output;
+            }
+        }
 
-		public void RemoveToChart()
-		{
+        public void AddToCart()
+        {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
-		}
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK - There should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel cartItem = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(cartItem);
+            }
 
-		public void CheckOut()
-		{
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+        }
 
-		}
-	}
+        public bool CanRemoveToChart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+
+                return output;
+            }
+        }
+
+        public void RemoveToChart()
+        {
+            NotifyOfPropertyChange(() => SubTotal);
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
 }
