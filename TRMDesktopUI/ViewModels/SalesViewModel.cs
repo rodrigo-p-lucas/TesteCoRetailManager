919b1fd109ecaecdc30c176ca20df90c043cc340
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-15 21:40:19 -0300

    Melhoria na lista de produtos da tela
    Exibe mais detalhes sobre os produtos na tela de vendas e altera calculo de imposto para usar Linq

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -118,13 +118,9 @@ namespace TRMDesktopUI.ViewModels
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate()/100;
 
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-                }
-            }
+            taxAmount = Cart
+                .Where(item => item.Product.IsTaxable)
+                .Sum(item => item.Product.RetailPrice * item.QuantityInCart * taxRate);
 
             return taxAmount;
         }
