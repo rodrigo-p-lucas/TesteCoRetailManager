2763a4dd7caf0ed8a28fef8d8b6dcb0e71cc83b4
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-15 21:09:40 -0300

    Alterar referencia de conteiner em ShellViewModel
    Alterar referencia de conteiner pela referencia de IoC interno do Carliburm na classe ShellViewModel

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -10,27 +10,22 @@ namespace TRMDesktopUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
-        public ShellViewModel(LoginViewModel loginVM, IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
-            _loginVM = loginVM;
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.Subscribe(this);
 
-            ActivateItem(_loginVM);
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
         {
             ActivateItem(_salesVM);
-            _loginVM = _container.GetInstance<LoginViewModel>();
         }
     }
 }
