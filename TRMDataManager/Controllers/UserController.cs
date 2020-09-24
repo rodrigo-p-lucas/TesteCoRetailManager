1726afb73491e0b329c2f8e9866d62a231532d95
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-24 17:38:38 -0300

    Implementar gestão de usuários
    Cria processo de gestão de permissões por roles dos usuários cadastrados, e atualiza dados via API.

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -59,5 +59,45 @@ namespace TRMDataManager.Controllers
 
             return output;
         }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        [Route("api/User/Admin/GetAllRoles")]
+        public Dictionary<string,string> GetAllRoles()
+        {
+            using(var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id, x => x.Name);
+                return roles;
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [Route("api/User/Admin/AddRole")]
+        public void AddRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [Route("api/User/Admin/RemoveRole")]
+        public void RemoveRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+            }
+        }
     }
 }
