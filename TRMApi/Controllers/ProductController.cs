8b28df9c0f3ff1f43a84aabda74ac0d88cb5f9f9
Rodrigo Pereira <rodrigo.mrpascal@gmail.com>
2020-09-27 20:05:34 -0300

    Adicionar novo projeto de API
    Adiciona novo projeto API utilizando .Net Core 3.1 e realiza adaptações.

---
+++ TRMApi/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Cashier")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IList<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+
+            return data.GetProducts();
+        }
+    }
+}
---
+++ TRMApi/Controllers/SaleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post(SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            data.SaveSale(sale, userId);
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSaleReport")]
+        public IList<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+            return data.GetSaleReport();
+        }
+    }
+}
